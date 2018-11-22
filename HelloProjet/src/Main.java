10fb573a226a73d786a74a0d77ad72207f640165
lizhongyuan123 <1065855908@qq.com>
2018-11-22 23:03:02 +0800

    h

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,11 +5,6 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-        System.out.println("第一次!");
-<<<<<<< HEAD
-        System.out.println("第二次!");
-=======
-        System.out.println("王五第一次!");
->>>>>>> d525ce5260ddecd43d205cb233ed9be32b975e45
+
     }
 }
